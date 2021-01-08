ffe70435020fcfcb3cf384ea2c1b86e718982373
pa waer <pawaer@t-online.de>
2021-01-08 12:44:35 +0100

    Adapted setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="??", # pip install ?? 
-    version="0.0.0",
-    author="??",
-    author_email="??",
-    description="??",
+    name="matrixmath_pawaer",
+    version="0.0.1",
+    author="pawaer",
+    author_email="pawaer@t-online.de",
+    description="First Package. Providing Matrix Calculations, such as multiplication",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="GitHub.com??",
+    url="github.com/pawaer/matrixmath",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='??', # e.g. >=3.6
+    python_requires='>=3.6',
 )
